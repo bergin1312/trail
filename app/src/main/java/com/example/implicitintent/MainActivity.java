faf2569fe295c7803a643ddc6046fa534957c926
BERGIN.S <119404594+bergin1312@users.noreply.github.com>
2024-02-28 14:44:46 +0530

    upload project

---
+++ app/src/main/java/com/example/implicitintent/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.implicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent=new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.geeksforgeeks.org/"));
+
+        startActivity(intent);
+
+
+    }
+}
